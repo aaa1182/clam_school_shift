32a8b1cb47a50886799ebb8a3f2cd90bbf197343
teke <takehiro.arai.jg@gmail.com>
2021-09-03 01:57:29 +0900

    create admin and teacher models, controlers by devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_scope :teacher do
+    root 'teachers/sessions#new'
+  end
+  devise_for :teachers, controllers: {
+    sessions: 'teachers/sessions',
+    passwords: 'teachers/passwords',
+    registrations: 'teachers/registrations'
+  }
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
